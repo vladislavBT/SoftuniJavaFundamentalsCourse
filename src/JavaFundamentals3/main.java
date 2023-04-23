d0867446d31825ab58e277d5abc3aaf0880b12a4
vladislavBT <vladislavtulilov12345678@gmail.com>
2023-04-23 19:22:08 +0300

    JavaFundamentals3

---
+++ src/JavaFundamentals3/main.java
@@ -0,0 +1,11 @@
+package JavaFundamentals3;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+
+       int n=0x1A;
+        System.out.println(n);
+    }
+}
